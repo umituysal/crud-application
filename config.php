e8c2cebf0582f042a6943f42328a4c611df6d8e3
Ümit Uysal <42471170+umituysal@users.noreply.github.com>
2019-03-31 23:48:08 +0300

    Update config.php

--- config.php
+++ config.php
@@ -9,6 +9,6 @@ try {
     $pdo=new PDO("mysql:host=".DB_SERVER.";dbname=".DB_NAME,DB_USERNAME,DB_PASSWORD);
     $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
-    die("ERROR:Could not connect.".$e->getMessage());
+    die("ERROR:Baglanamadı.".$e->getMessage());
 }
 ?>
